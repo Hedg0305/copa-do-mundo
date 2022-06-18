eda01a9de45d20e8d069ec84055fd8a3c02a13f4
Lucas Ferreira <lucas.simoes.ferreira@gmail.com>
2022-06-18 19:31:08 -0300

    Add: comittee

---
+++ src/components/TechnicalComittee/index.tsx
@@ -0,0 +1,40 @@
+import { Avatar, Box } from '@chakra-ui/react';
+import React from 'react'
+
+interface ComitteeProps {
+  passport: string;
+  name: string;
+  age: number;
+  birthdate: string;
+  role: string;
+}
+
+const TechnicalComittee = ({ birthdate, passport, name, age, role }: ComitteeProps) => {
+
+  return (
+    <Box p="15px" w='400px' borderWidth='1px' borderRadius='lg' borderColor='gray.300' overflow='hidden' bg="whiteAlpha.700" fontSize="18px">
+      <Box display="flex" alignItems="center" justifyContent="space-between">
+        <Avatar name={name} />
+        <Box
+          fontWeight='Bold'
+          as='h2'
+          lineHeight='tight'
+          color="blackAlpha.700"
+        >
+          {name}
+          <Box>
+            {age} anos
+          </Box>
+        </Box>
+        <Box display="flex" justifyContent="center" flexDirection="column" color="blackAlpha.800">
+          {birthdate}
+          <div>
+            {role}
+          </div>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default TechnicalComittee;
--- src/pages/[edition]/[team].tsx
+++ src/pages/[edition]/[team].tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { GetServerSideProps, GetStaticProps } from "next";
-import { Box, Center, Flex, Heading, VStack } from "@chakra-ui/react";
-import Bracket from "../../components/Bracket";
+import { GetServerSideProps } from "next";
+import { Box, Center, Grid, Heading, VStack } from "@chakra-ui/react";
+import Player from "../../components/PlayerCard";
+import TechnicalComittee from "../../components/TechnicalComittee"
 
 interface ServerSideProps {
   edition: string;
@@ -12,20 +13,77 @@ interface PageProps {
   hostCountry: string;
   year: number;
   team: string;
+  players: Player[];
+  technicalComittee: Comittee[];
+
+}
+interface Comittee {
+  passport: string;
+  name: string;
+  age: number;
+  birthdate: string;
+  role: string;
 }
 
-const Competition = ({ hostCountry, year, team }: PageProps) => {
+interface Player {
+  passport: string;
+  birthdate: string;
+  name: string;
+  age: number;
+  position: string;
+  stats: {
+    defenses?: number;
+    sufferedGoals?: number;
+    goals?: number;
+    assists?: number;
+    yellowCards: number;
+    redCards: number;
+  }
+}
+
+const Competition = ({ hostCountry, year, team, technicalComittee, players }: PageProps) => {
   return (
-    <Box bgColor='gray.400'>
+    <Box bgColor='gray.400' h="100vh">
       <Center>
         <VStack>
           <Heading as='h1' size='2xl' mb='10'>
             Copa do mundo - {hostCountry} - {year}
           </Heading>
-
-          <Heading as='h2' size='lg' mb='10'>
+          <Heading as='h2' size='lg'>
             {team}
           </Heading>
+          <Heading as='h3' size='md'>
+            Jogadores
+          </Heading>
+          <Grid templateColumns='repeat(2, 1fr)' gap="35px">
+            {players.map((player) => (
+              <Player
+                key={player.passport}
+                passport={player.passport}
+                birthdate={player.birthdate}
+                name={player.name}
+                age={player.age}
+                position={player.position}
+                stats={player.stats}
+              />
+            )
+            )}
+          </Grid>
+          <Heading as='h3' size='md'>
+            Comissão Técnica
+          </Heading>
+          <Grid templateColumns='repeat(2, 1fr)' gap="35px">
+            {technicalComittee.map((comittee) => (
+              <TechnicalComittee
+                passport={comittee.passport}
+                name={comittee.name}
+                age={comittee.age}
+                key={comittee.passport}
+                birthdate={comittee.birthdate}
+                role={comittee.role}
+              />
+            ))}
+          </Grid>
         </VStack>
       </Center>
     </Box>
@@ -44,7 +102,41 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       year,
       hostCountry,
       team,
+      technicalComittee: [{
+        passport: "56467987451",
+        name: "Hernan Gomez",
+        age: 45,
+        birthdate: "12/06/1985",
+        role: "Técnico"
+      }],
+      players: [
+        {
+          passport: "231654564",
+          birthdate: "12/12/1995",
+          name: "Lionel Messi",
+          age: 34,
+          position: "PE",
+          stats: {
+            goals: 13,
+            assists: 5,
+            yellowCards: 1,
+            redCards: 0,
+          }
+        },
+        {
+          passport: "32132131",
+          birthdate: "12/12/1999",
+          name: "Emiliano Martinez",
+          age: 29,
+          position: "GOL",
+          stats: {
+            defenses: 34,
+            sufferedGoals: 5,
+            yellowCards: 0,
+            redCards: 0,
+          }
+        }
+      ]
     },
   };
 };
-
